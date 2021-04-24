be974d934593e74b2b6205fd43d771deb29ac9fd
Dipesh Sakharkar <dipeshsakharkar1@gmail.com>
2021-04-24 20:30:07 +0530

    Home and About Page

--- src/Screens/Home.js
+++ src/Screens/Home.js
@@ -1,9 +1,16 @@
 import React from 'react'
-
+import CommonComp from '../Components/CommonComp'
+import HomeImg from '../Images/Laptop-rb.png'
+import './Home.css'
 const Home =()=>{
   return(
     <>
-    <h1>Welcome to Home.js</h1>
+    <CommonComp
+      name="Grow your business with"
+      imgSrc={HomeImg}
+      visit="/services"
+      btname="Get Started"
+      />
     </>
   )
 }
